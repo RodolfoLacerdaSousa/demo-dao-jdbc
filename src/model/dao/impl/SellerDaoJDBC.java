a0ffbda06b56bba554bd078d1f09917d65d87471
RodolfoLacerdaSousa <rodolfolacerdajp@gmail.com>
2021-05-11 16:02:31 -0300

    findByDepartment implemetation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -94,6 +97,54 @@ public class SellerDaoJDBC implements SellerDao {
 	public List<Seller> findAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*, department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE DepartmentId = ? "
+					+ "ORDER BY Name");
+			
+			st.setInt(1, department.getId());
+			rs = st.executeQuery();
+			
+			List<Seller> list =new ArrayList<>();
+			
+			// controle para nao repetir o departamento, para nao criar 1 departamento para cada objeto q achar no While
+			Map<Integer, Department> map = new HashMap<>();  // criou 1 map vazio, e vai guardar dentro desse map qualquer departamento que intanciar
+			
+			//TOP ISSO AQUI
+			while(rs.next()) { //aqui foi WHILE pq pode ter varios resultados, ai enquanto tiver ele vai percorrendo
+				
+				// Antes de criar o departamento, vai testar se ele ja existe em cada vez q passar no while
+				Department dep = map.get(rs.getInt("DepartmentId")); //Vai no Map e tenta buscar com o Get um departamento que tenha o mesmo ID
+				
+				if(dep == null) { //siginfica q na busca la do MAP nao tinha esse ID ainda... ai sim vai criar 1 novo department
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep); //para salvar no MAP o departamento e assim nao criar de novo.
+				}
+				
+				Seller obj = instatiateSeller(rs, dep);
+				
+				list.add(obj);
+			}
+			return list;	
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs); 
+			//nao precisa fechar a conexao conn
+		}
 	}
 
 }
